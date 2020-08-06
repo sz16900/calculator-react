2fab249189f8951ff9a3d40658657be20a1ae37e
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-06 18:19:15 -0500

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// My Components
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+};
+
+export default App;

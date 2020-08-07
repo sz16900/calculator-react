c1b29a5ed3db5d193d1ba5e174f3da79f3eca0b9
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-07 12:13:22 -0500

    refactor button styles with style-components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ButtonStyled = styled.button`
+  width: ${(props) => (props.wide ? '50%' : '25%')};
+  height: 100%;
+  font-size: 2em;
+  font-weight: bolder;
+  border: 1px solid black;
+  background: ${(props) => (props.color ? '#F2913E' : '#E0E0E0')};
+`;
 
 const Button = (props) => {
-  const { name } = props;
+  const { name, wide, color } = props;
   return (
-    <button id="button-container" type="button">
-      {name}
-    </button>
+    <ButtonStyled wide={wide} color={color}>
+      <p>{name}</p>
+    </ButtonStyled>
   );
 };
 
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,30 +8,30 @@ const ButtonPanel = () => (
       <Button name="AC" />
       <Button name="+/-" />
       <Button name="%" />
-      <Button name="+" />
+      <Button name="+" color />
     </div>
     <div className="group-2">
       <Button name="7" />
       <Button name="8" />
       <Button name="9" />
-      <Button name="x" />
+      <Button name="x" color />
     </div>
     <div className="group-3">
       <Button name="4" />
       <Button name="5" />
       <Button name="6" />
-      <Button name="-" />
+      <Button name="-" color />
     </div>
     <div className="group-4">
       <Button name="1" />
       <Button name="3" />
       <Button name="4" />
-      <Button name="+" />
+      <Button name="+" color />
     </div>
     <div className="group-5">
-      <Button name="0" />
+      <Button name="0" wide />
       <Button name="," />
-      <Button name="=" />
+      <Button name="=" color />
     </div>
   </div>
 );

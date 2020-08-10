72323b8277d6fd16d10e7229826e5410bef3954c
sz16900 <sz16900@my.bristol.ac.uk>
2020-08-10 15:07:49 -0500

    add handleClick func to clas App

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,34 @@ import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import '../assets/main.css';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <div id="app-container">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+  handleClick(buttonName) {
+    const { copyState } = this.state;
+    const resultObject = calculate(copyState, buttonName);
+    this.setState({
+      resultObject,
+    });
+  }
+  render() {
+    const copyObject = this.state;
+    return (
+      <div id="app-container">
+        <Display result={copyObject.total || copyObject.next || '0'} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
